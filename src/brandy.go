25c20668a32da769e75c9c4a8d14d0fb48ec75fd
Miclle <miclle.zheng@gmail.com>
2015-10-17 01:39:35 +0800

    Update brandy main package.

--- src/brandy.go
+++ src/brandy.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"gopkg.in/fsnotify.v1"
+	"log"
+	"os"
+	// "gopkg.in/fsnotify.v1"
 )
 
 func main() {
+	var str string
+	if len(os.Args) > 1 {
+		str = os.Args[1]
+	}
+	log.Println(str)
 
+	switch str {
+	case "help", "h":
+		log.Println("help task")
+
+	case "clean", "c":
+		log.Println("clean task")
+
+	case "build", "b":
+		log.Println("build task")
+
+	case "server", "s":
+		log.Println("server task")
+
+	case "verbose", "v":
+		log.Println("verbose task")
+
+	case "version", "V":
+		log.Println("version task")
+	default:
+		log.Println("default task")
+	}
 }
