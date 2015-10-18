4789575bd9acc9da92bd37c085a2101b24b1c951
Miclle <miclle.zheng@gmail.com>
2015-10-18 10:35:50 +0800

    Update brandy main package

---
+++ src/brandy/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"brandy/conf"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	// "gopkg.in/fsnotify.v1"
+)
+
+func main() {
+
+	conf.Init()
+
+	var str string
+
+	if len(os.Args) > 1 {
+		str = os.Args[1]
+
+		switch str {
+		case "help", "h":
+			log.Println("help task")
+
+		case "new", "n":
+			log.Println("new task")
+
+		case "clean", "c":
+			log.Println("clean task")
+
+		case "build", "b":
+			log.Println("build task")
+
+		case "server", "s":
+			log.Println("server task")
+
+		case "verbose", "v":
+			log.Println("verbose task")
+
+		case "version", "V":
+			log.Println("version task")
+
+		default:
+			log.Println("default task")
+		}
+	}
+
+	log.Println("[brandy] listening on port", conf.Port)
+	log.Println("[brandy] Serving files from", conf.Dir)
+
+	addr := fmt.Sprintf(":%d", conf.Port)
+	handler := http.FileServer(http.Dir(conf.Dir))
+	panic(http.ListenAndServe(addr, handler))
+}
