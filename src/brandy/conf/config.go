a4de2c82f0d680f06b9c840ff710d014f75e7e36
Miclle <miclle.zheng@gmail.com>
2015-10-18 10:34:15 +0800

    Update conf package Init method.

--- src/brandy/conf/config.go
+++ src/brandy/conf/config.go
@@ -2,12 +2,26 @@ package conf
 
 import (
 	"github.com/namsral/flag"
+	"log"
+	"os"
+	"path/filepath"
 )
 
+var Dir string
 var Port int
 
 func Init() {
+	flag.StringVar(&Dir, "dir", "", "dir of brandy")
 	flag.IntVar(&Port, "port", 3000, "port of brandy")
-	flag.IntVar(&Port, "p", 3000, "port of brandy")
 	flag.Parse()
+
+	// Get the directory of the currently
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if Dir == "" {
+		Dir = dir
+	}
 }
