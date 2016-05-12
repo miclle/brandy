04eabed974e977849d05483d22edef58369c0182
Miclle Zheng <miclle.zheng@gmail.com>
2016-05-13 00:42:46 +0800

    Add logger package

---
+++ logger/logger.go
@@ -0,0 +1,21 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/op/go-logging"
+)
+
+// Log is globle logger
+var Log = logging.MustGetLogger("brandy")
+
+var format = logging.MustStringFormatter(`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`)
+
+func init() {
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+}
