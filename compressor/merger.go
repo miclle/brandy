818a8e6888c33e36bf8f03e4d2439f0c33f0a027
Miclle Zheng <miclle.zheng@gmail.com>
2016-05-15 21:35:26 +0800

    Update merger compressor

--- compressor/merger.go
+++ compressor/merger.go
@@ -1,6 +1,12 @@
 package compressor
 
+import "errors"
+
 // Merger func is the joining together of some files so that they become one.
-func Merger() {
+func Merger(dist string, srcs ...string) error {
 	// TODO
+	if len(srcs) <= 0 {
+		return errors.New("Source files must require")
+	}
+	return nil
 }
