b6ab9afa79870467403234c7006d5e2a06e48fbf
Miclle Zheng <miclle.zheng@gmail.com>
2016-05-16 23:16:06 +0800

    Add minify unit test

--- compressor/minify_test.go
+++ compressor/minify_test.go
@@ -1,7 +1,39 @@
 package compressor
 
-import "testing"
+import (
+	"bufio"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMinify(t *testing.T) {
-	// TODO
+	assert := assert.New(t)
+	src := "../test/compiler.css"
+	dist := "../test/compiler.min.css"
+
+	r, err := os.Open(src)
+	assert.Nil(err)
+	assert.NotNil(r)
+	assert.NotEmpty(r)
+
+	f, err := os.Create(dist)
+	assert.Nil(err)
+
+	w := bufio.NewWriter(f)
+
+	err = Minify(MediaTypeTextCSS, w, r)
+	assert.Nil(err)
+	assert.Nil(w.Flush())
+
+	srcFileInfo, err := os.Stat(src)
+	assert.Nil(err)
+	assert.NotNil(srcFileInfo)
+
+	distFileInfo, err := os.Stat(dist)
+	assert.Nil(err)
+	assert.NotNil(distFileInfo)
+
+	assert.True(srcFileInfo.Size() >= distFileInfo.Size())
 }
